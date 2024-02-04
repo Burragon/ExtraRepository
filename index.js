430fb2e4cb95115014fbf3ba9afa82405ac3b031
Burragon <147354335+Burragon@users.noreply.github.com>
2024-02-03 22:38:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function calc() {
   const CurrentTotal = document.getElementById("total");
   let NewTotal = Number(CurrentTotal.value);
-  const Result = NewTotal + 1
-  CurrentTotal.textContent = Result
+  const Result = NewTotal + 1;
+  CurrentTotal.textContent = Result;
 }
